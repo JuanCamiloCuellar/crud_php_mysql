d6b21b69a1731e131b357243b6f2e01f6b8ab084
Juan Camilo Cuellar <jukas1713@gmail.com>
2023-03-04 18:22:03 -0500

    modulo inter
    se crea una pagina inter.php para solo el modulo de login

---
+++ includes/inter.php
@@ -0,0 +1,26 @@
+<div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                <div class="card-body p-5 text-center">
+                    <div class="mb-md-5 mt-md-4 pb-5">
+                        <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                        <form method="post">
+                            <div class="form-outline form-white mb-4">
+                                <input type="text" class="form-control form-control-lg" placeholder="Usuario"  name="username">
+                            </div>
+                            <div class="form-outline form-white mb-4">
+                                <input type="password" class="form-control form-control-lg" placeholder="Password"  name="password">
+                            </div>
+                            <button class="btn btn-outline-light btn-lg px-5" type="submit">Iniciar sesión</button>
+                        </form>
+                    </div>
+                    <?php echo "<br>" ?>
+                    <?php if (isset($error_message)) { ?>
+                        <p><?php echo $error_message; ?></p>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
